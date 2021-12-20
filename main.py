1f196ac60f0c02ff6fe1e6d3445cff3111c26691
Emre Cenk <emrecenk9@gmail.com>
2021-12-20 14:58:32 -0500

    created cookie cutter discord bot

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 
-import discord
+
+
 """
 to add the bot: https://discord.com/api/oauth2/authorize?client_id=922575933515395092&permissions=19520&scope=bot
 """
+
+import discord
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
+
 if __name__ == '__main__':
     from dotenv import load_dotenv
     load_dotenv()
     from os import environ
-    a = environ["DISCORD_TOKEN"]
-    print(a)
+    token = environ["DISCORD_TOKEN"]
+    client = MyClient()
+    client.run(token)
