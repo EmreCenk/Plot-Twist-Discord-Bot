1107a4b6c10253afe1f9f396a559a40e55ef3a8c
Emre Cenk <emrecenk9@gmail.com>
2021-12-20 14:55:58 -0500

    generated url to invite bot

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
+import discord
+"""
+to add the bot: https://discord.com/api/oauth2/authorize?client_id=922575933515395092&permissions=19520&scope=bot
+"""
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    from dotenv import load_dotenv
+    load_dotenv()
+    from os import environ
+    a = environ["DISCORD_TOKEN"]
+    print(a)
